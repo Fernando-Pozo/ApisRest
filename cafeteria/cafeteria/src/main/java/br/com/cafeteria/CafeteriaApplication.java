5eb6513c489752898fbc541ce15d9bed39f39f3d
Fernando-Pozo <fernandopozomqs@gmail.com>
2023-11-03 01:10:08 -0300

    swagger

--- cafeteria/cafeteria/src/main/java/br/com/cafeteria/CafeteriaApplication.java
+++ cafeteria/cafeteria/src/main/java/br/com/cafeteria/CafeteriaApplication.java
@@ -2,9 +2,18 @@ package br.com.cafeteria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
+@Controller
 public class CafeteriaApplication {
+
+	@GetMapping("/")
+	public ModelAndView swaggerUi() {
+		return new ModelAndView("redirect:/swagger-ui/");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CafeteriaApplication.class, args);
